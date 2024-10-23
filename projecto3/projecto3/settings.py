39646ecb37d203e40292a67d7acf60b9a21ae7f3
PykeSub <gastonalvarez0023@gmail.com>
2024-10-23 09:44:12 -0400

    Detalle

--- projecto3/projecto3/settings.py
+++ projecto3/projecto3/settings.py
@@ -78,7 +78,7 @@ pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_demo_emp2',
         'USER': 'root',
         'PASSWORD': '',
